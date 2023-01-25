77c86890a820504df61a7a16be72554f664174fc
Vinicius Frota <viniciuspxfrota@gmail.com>
2023-01-25 12:18:12 -0300

    setup electron project

---
+++ public/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+require('@electron/remote/main').initialize();
+
+function createWindow() {
+    const window = new BrowserWindow( {
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    });
+
+    window.loadURL('http://localhost:3000');
+};
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin')
+        app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0)
+        createWindow();
+});
